9400df426ab1469197d08b83d062254fc7de9dd6
marianahiath <maarianahiath@gmail.com>
2021-06-28 20:59:57 -0300

    like finished, added remove button

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import { RoomCode } from '../components/RoomCode';
 import { Button } from '../components/Button';
@@ -8,6 +9,7 @@ import { Question } from '../components/Question';
 
 //import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 
@@ -21,6 +23,12 @@ export function AdminRoom() {
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId);
+
+    async function handleDeleteQuestion(questionId: string) {
+       if (window.confirm('Tem certeza que deseja excluir essa pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+       }
+    }
 
     return (
         <div id="page-room">
@@ -46,7 +54,15 @@ export function AdminRoom() {
                                 key={question.id}  
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="Remover pergunta" />
+                                </button>
+
+                            </Question>
                         );
                     })}
                 </div>
