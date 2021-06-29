3d477a64f15b08b38192330e167547a30df22742
marianahiath <maarianahiath@gmail.com>
2021-06-28 21:33:45 -0300

    close room added

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
@@ -19,10 +19,19 @@ type RoomParams = {
 
 export function AdminRoom() {
     //const { user }  = useAuth();
+    const history = useHistory();
     const params = useParams<RoomParams>();
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId);
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date(),
+        })
+
+        history.push('/');
+    }
 
     async function handleDeleteQuestion(questionId: string) {
        if (window.confirm('Tem certeza que deseja excluir essa pergunta?')) {
@@ -37,7 +46,12 @@ export function AdminRoom() {
                     <img src={logoImg} alt="Letmeask" />
                     <div>
                         <RoomCode code={roomId}/>
-                        <Button isOutLined>Encerrar Sala</Button>
+                        <Button
+                            isOutLined
+                            onClick={handleEndRoom}
+                        >
+                            Encerrar Sala
+                        </Button>
                     </div>
                 </div>
             </header>
